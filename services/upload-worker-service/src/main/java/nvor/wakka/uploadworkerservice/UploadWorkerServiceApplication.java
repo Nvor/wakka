9e55c00cdaa0dba6e66a6f6aa6b74a204ac2f150
nvor <nickbaripour@gmail.com>
2022-03-25 19:33:19 -0400

    Start upload worker service

---
+++ services/upload-worker-service/src/main/java/nvor/wakka/uploadworkerservice/UploadWorkerServiceApplication.java
@@ -0,0 +1,13 @@
+package nvor.wakka.uploadworkerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadWorkerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadWorkerServiceApplication.class, args);
+	}
+
+}
---
+++ services/upload-worker-service/src/test/java/nvor/wakka/uploadworkerservice/UploadWorkerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nvor.wakka.uploadworkerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadWorkerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
